941fd1373e6272b53e311c5e769020e7271da65b
Fabio Bove <fabio.bove.dr@gmail.com>
2024-03-10 12:08:06 +0100

    Update init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -1,3 +1,3 @@
 # Run the setup script to create the DB and the schema in the DB
 # Note: make sure that your password matches what is in the Dockerfile
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P password123!  -i init.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P password123!  -i init_db.sql
